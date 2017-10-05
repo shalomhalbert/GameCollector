f3f87941d967b37582b556266c09d2441e8af44a
Shalom Halbert <shalomhalbert@gmail.com>
2017-10-05 10:28:29 -0400

    Added MainActivity for managing Personal Collection screen

---
+++ app/src/main/java/com/example/android/gamecollector/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.gamecollector;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shalom on 2017-10-05.
+ */
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
