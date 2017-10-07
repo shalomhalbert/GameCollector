5c3b86c19e626492cb0072439a39b558895bebb6
Shalom Halbert <shalomhalbert@gmail.com>
2017-10-07 13:14:11 -0400

    File has constants for Collectible database schema
    Contains Uri, table name, and column constants

---
+++ app/src/main/java/com/example/android/gamecollector/data/CollectiblesContract.java
@@ -0,0 +1,41 @@
+package com.example.android.gamecollector.data;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by shalom on 2017-10-07.
+ * Contract for storing collectables data
+ */
+
+public final class CollectiblesContract {
+    public static final String CONTENT_AUTHORITY = "com.example.android.gamecollector";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_VIDEO_GAMES = "video_games";
+
+    /*Defines table contents for the video_games table*/
+    public static class VideoGamesEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_VIDEO_GAMES);
+
+        /*Table name*/
+        public static final String TABLE_NAME = "video_games";
+
+        /* Storage class: INTEGER
+         * Table constraints: PRIMARY KEY, AUTOINCREMENT */
+        public static final String COLUMN_ID = BaseColumns._ID;
+        /* Storage class: TEXT
+         * Table constraints: NOT NULL */
+        public static final String COLUMN_CONSOLE = "console";
+        /* Storage class: TEXT
+         * Table constraints: NOT NULL */
+        public static final String COLUMN_TITLE = "title";
+        /* Storage class: TEXT
+         * Table constraints: DEFAULT[unknown] */
+        public static final String COLUMN_LICENSEE = "licensee";
+        /* Storage class: TEXT
+         * Table constraints: DEFAULT[unknown] */
+        public static final String COLUMN_RELEASED = "released";
+
+        //Add MIME type Strings
+    }
+}
