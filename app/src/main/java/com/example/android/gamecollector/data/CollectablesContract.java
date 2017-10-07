d75ed98cabc7bf2f224315bac7f57663bea1362d
Shalom Halbert <shalomhalbert@gmail.com>
2017-10-07 14:09:57 -0400

    Refactored class's name + fixed table constraints
    Name was misspelled.
    Removed square brackets from DEFAULT table constraints.

--- app/src/main/java/com/example/android/gamecollector/data/CollectablesContract.java
+++ app/src/main/java/com/example/android/gamecollector/data/CollectablesContract.java
@@ -8,21 +8,21 @@ import android.provider.BaseColumns;
  * Contract for storing collectables data
  */
 
-public final class CollectiblesContract {
+public final class CollectablesContract {
 
     /*Should be empty*/
-    private CollectiblesContract() {}
+    private CollectablesContract() {}
 
     public static final String CONTENT_AUTHORITY = "com.example.android.gamecollector";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
-    public static final String PATH_VIDEO_GAMES = "video_games";
+    public static final String PATH_VIDEO_GAMES = "videoGames";
 
     /*Defines table contents for the video_games table*/
     public static class VideoGamesEntry implements BaseColumns {
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_VIDEO_GAMES);
 
         /*Table name*/
-        public static final String TABLE_NAME = "video_games";
+        public static final String TABLE_NAME = "videoGames";
 
         /* Storage class: INTEGER
          * Table constraints: PRIMARY KEY, AUTOINCREMENT */
@@ -34,10 +34,10 @@ public final class CollectiblesContract {
          * Table constraints: NOT NULL */
         public static final String COLUMN_TITLE = "title";
         /* Storage class: TEXT
-         * Table constraints: DEFAULT[unknown] */
+         * Table constraints: DEFAULT unknown */
         public static final String COLUMN_LICENSEE = "licensee";
         /* Storage class: TEXT
-         * Table constraints: DEFAULT[unknown] */
+         * Table constraints: DEFAULT unknown */
         public static final String COLUMN_RELEASED = "released";
 
         //Add MIME type Strings
