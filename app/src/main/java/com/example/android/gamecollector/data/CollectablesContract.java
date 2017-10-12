99b459d0f88f05b92428d033345237b4d09d88ac
Shalom Halbert <shalomhalbert@gmail.com>
2017-10-12 14:17:47 -0400

    Modified table name, and added new table
    Table name now uses underscores instead of camelCase.
    New table is used for its full-text search functionality to make
    finding a video game more efficient.

--- app/src/main/java/com/example/android/gamecollector/data/CollectablesContract.java
+++ app/src/main/java/com/example/android/gamecollector/data/CollectablesContract.java
@@ -16,14 +16,14 @@ public final class CollectablesContract {
 
     public static final String CONTENT_AUTHORITY = "com.example.android.gamecollector";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
-    public static final String PATH_VIDEO_GAMES = "videoGames";
+    public static final String PATH_VIDEO_GAMES = "video_games";
+    public static final String PATH_FTS_VIDEO_GAMES = "fts_video_games";
 
     /*Defines table contents for the video_games table*/
-    public static class VideoGamesEntry implements BaseColumns {
+    public static final class VideoGamesEntry implements BaseColumns {
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_VIDEO_GAMES);
 
-        /*Table name*/
-        public static final String TABLE_NAME = "videoGames";
+        public static final String TABLE_NAME = "video_games";
 
         /* Storage class: INTEGER
          * Table constraints: PRIMARY KEY, AUTOINCREMENT */
@@ -48,5 +48,30 @@ public final class CollectablesContract {
         /*MIME type for an individual video game*/
         public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/"
                 + CONTENT_AUTHORITY + "/" + PATH_VIDEO_GAMES;
+    }
+
+    /*Defines table contents for the fts_video_games table which enables full-text search implementation
+    * on the video_games table.
+    * Indexes the 'title' column from video_games table for faster full-text searching*/
+    public static final class FtsVideoGamesEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_FTS_VIDEO_GAMES);
+
+        public static final String TABLE_NAME = "fts_video_games";
+
+        /*Full-text search version is fts4*/
+        public static final String FTS_VERSION = "fts4";
+
+        /*Used in CREATE VIRTUAL TABLE statement to show the first column should equal video_games table's row ids*/
+        public static final String COLUMN_DOC_ID = "docid";
+        /*This is the column from video_games table that we want to implement full-text search on*/
+        public static final String COLUMN_TITLE = VideoGamesEntry.COLUMN_TITLE;
+
+        /*MIME type for a list from fts_video_games*/
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/"
+                + CONTENT_AUTHORITY + "/" + PATH_FTS_VIDEO_GAMES;
+
+        /*MIME type for an single row*/
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/"
+                + CONTENT_AUTHORITY + "/" + PATH_FTS_VIDEO_GAMES;
     }
 }
