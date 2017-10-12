5a7be54635b17a887baffcc0f113b4a55cebfee1
Shalom Halbert <shalomhalbert@gmail.com>
2017-10-12 14:21:14 -0400

    Added SQL statement constants and modified methods
    SQL statements are for creating, populating, and updating fts4
    (full-text search 4/ fts_video_games) table.
    Added method for parsing .csv files.

--- app/src/main/java/com/example/android/gamecollector/data/CollectablesDbHelper.java
+++ app/src/main/java/com/example/android/gamecollector/data/CollectablesDbHelper.java
@@ -5,7 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.android.gamecollector.ParseCSV;
-import com.example.android.gamecollector.data.CollectablesContract.VideoGamesEntry;
+import com.example.android.gamecollector.data.CollectablesContract.*;
 
 import java.util.List;
 
@@ -20,16 +20,33 @@ public class CollectablesDbHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "collector_opportunities.db";
     /*Database version as of 2017-10-07*/
     public static final int DATABASE_VERION = 1;
-    /*Used for creating */
-    public static final String SQL_CREATE_VIDEO_GAMES_TABLE = "CREATE TABLE "
+
+    /*Used for creating table with all data*/
+    private static final String SQL_CREATE_VIDEO_GAMES_TABLE = "CREATE TABLE "
             + VideoGamesEntry.TABLE_NAME + " ("
             + VideoGamesEntry.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + VideoGamesEntry.COLUMN_CONSOLE + " TEXT NOT NULL, "
             + VideoGamesEntry.COLUMN_TITLE + " TEXT NOT NULL, "
             + VideoGamesEntry.COLUMN_LICENSEE + " TEXT DEFAULT unknown, "
             + VideoGamesEntry.COLUMN_RELEASED + " TEXT DEFAULT unknown);";
-    private static final String SQL_DELETE_VIDEO_GAMES_TABLE = "DROP TABLE IF EXISTS "
-            + VideoGamesEntry.TABLE_NAME;
+    /*Deletes video_games table*/
+    private static final String SQL_DELETE_VIDEO_GAMES_TABLE = "DROP TABLE IF EXISTS " + VideoGamesEntry.TABLE_NAME;
+
+    /*Used for creating virtual table that uses fts4*/
+    private static final String SQL_CREATE_FTS_VIDEO_GAMES_TABLE = "CREATE VIRTUAL TABLE "
+            + FtsVideoGamesEntry.TABLE_NAME + " USING " + FtsVideoGamesEntry.FTS_VERSION
+            + " (" + "content='" + VideoGamesEntry.TABLE_NAME + "'" + ", "
+            + FtsVideoGamesEntry.COLUMN_TITLE + ");";
+    /*Used for populating fts_video_games table with data from video_games table*/
+    private static final String SQL_POPULATE_FTS_VIDEO_GAMES_TABLE = "INSERT INTO "
+            + FtsVideoGamesEntry.TABLE_NAME + " (" + FtsVideoGamesEntry.COLUMN_DOC_ID + ", "
+            + FtsVideoGamesEntry.COLUMN_TITLE + ") SELECT " + VideoGamesEntry.COLUMN_ID + ", "
+            + VideoGamesEntry.COLUMN_TITLE + " FROM " + VideoGamesEntry.TABLE_NAME + ";";
+    /*Rebuilds fts_video_games table everytime the video_games table is updated*/
+    private static final String SQL_REBUILD_FTS_VIDEO_GAMES_TABLE = "INSERT INTO "
+            + FtsVideoGamesEntry.TABLE_NAME + "(" + FtsVideoGamesEntry.TABLE_NAME
+            + ") VALUES ('rebuild')";
+
     private ParseCSV parseCSV;
     private Context context;
 
@@ -39,9 +56,24 @@ public class CollectablesDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        /*Create and populate video_games table*/
         db.execSQL(SQL_CREATE_VIDEO_GAMES_TABLE);
+        populateVideoGames(db);
 
-        /*Parse .csv files and add each line to the database*/
+        /*Create and populate fts_video_games table*/
+        db.execSQL(SQL_CREATE_FTS_VIDEO_GAMES_TABLE);
+        db.execSQL(SQL_POPULATE_FTS_VIDEO_GAMES_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_VIDEO_GAMES_TABLE);
+        db.execSQL(SQL_REBUILD_FTS_VIDEO_GAMES_TABLE);
+    }
+
+    /*Parse .csv files and add each line to the database*/
+    private void populateVideoGames(SQLiteDatabase db) {
         parseCSV = new ParseCSV(context.getApplicationContext());
 
         for(List<String> game : parseCSV.parseFiles()) {
@@ -59,10 +91,5 @@ public class CollectablesDbHelper extends SQLiteOpenHelper {
 
             db.execSQL(SQL_INSERT_VIDEO_GAMES);
         }
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(SQL_DELETE_VIDEO_GAMES_TABLE);
     }
 }
