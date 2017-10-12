ca214af49d671e0b8c997f08ded7162eaa0aff20
Shalom Halbert <shalomhalbert@gmail.com>
2017-10-12 14:13:20 -0400

    Removed unused method and overrode another method
    Didn't use method for giving FloatingActionButton function because
    button was removed.
    Overrode  onCreateOptionsMenu() to:
    a) Inflate res/menu/options_menu.xml
    which adds the search widget to the action bar.
    b) Associate 'searchable configuration' with the SearchView.

--- app/src/main/java/com/example/android/gamecollector/MainActivity.java
+++ app/src/main/java/com/example/android/gamecollector/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.android.gamecollector;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -28,11 +32,17 @@ public class MainActivity extends AppCompatActivity{
 
     }
 
-    /*
-     * Directs onClick attribute defined for the FloatingActionButton in activity_main.xml
-     */
-    public void collectionAddition(View target) {
-//        Intent intent = new Intent(MainActivity.this, /*Add intent if used*/);
-//        startActivity(intent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /*Inflates res/menu/options_menu.xml which adds the search widget to the action bar*/
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        /*Associate 'searchable configuration' with the SearchView*/
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search_add_collectable).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
     }
 }
