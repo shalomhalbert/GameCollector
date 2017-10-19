5ef5eeb902037689728280aa13c4a8a2095ef38b
Shalom Halbert <shalomhalbert@gmail.com>
2017-10-19 13:47:55 -0400

    Renamed activity

--- app/src/main/java/com/example/android/gamecollector/AddCollectableActivity.java
+++ app/src/main/java/com/example/android/gamecollector/AddCollectableActivity.java
@@ -5,14 +5,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.widget.ListView;
 
-import com.example.android.gamecollector.data.CollectablesContract.*;
-import com.example.android.gamecollector.data.VideoGameCursorAdaptor;
+import com.example.android.gamecollector.data.sqlite.CollectablesCursorAdaptor;
+import com.example.android.gamecollector.data.sqlite.CollectablesSQLContract.FtsVideoGamesEntry;
+import com.example.android.gamecollector.data.sqlite.CollectablesSQLContract.VideoGamesEntry;
 
 /**
  * Created by shalom on 2017-10-11.
@@ -20,29 +24,31 @@ import com.example.android.gamecollector.data.VideoGameCursorAdaptor;
  * Uses search widget.
  */
 
-public class AddCollectableSearchResultsActivity extends AppCompatActivity {
+public class AddCollectableActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
     /*Use in Log statements*/
-    public static final String LOG_TAG = AddCollectableSearchResultsActivity.class.getSimpleName();
+    public static final String LOG_TAG = AddCollectableActivity.class.getSimpleName();
     /*Constant for intent.putExtra() in SearchView.OnQueryTextListener*/
     private static final String SEARCH_QUERY= "query";
 
-    /*Instaniated to help manage VideoGameCursorAdaptor*/
-    VideoGameCursorAdaptor videoGameCursorAdaptor;
+    /*Instaniated to help manage CollectablesCursorAdaptor*/
+    CollectablesCursorAdaptor collectablesCursorAdaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_collectable_search_results);
+        setContentView(R.layout.activity_add_collectable);
 
         /*Ensures only necessary columns are given to Cursor Adaptor*/
         String[] projection = {VideoGamesEntry.COLUMN_ID, VideoGamesEntry.COLUMN_CONSOLE, VideoGamesEntry.COLUMN_TITLE};
         /*Requests all data in the video_games table and is displayed in an identical order to their row number's*/
-        Cursor queryForWholeTable = getContentResolver().query(VideoGamesEntry.CONTENT_URI, projection, null, null, null, null);
+        Cursor getTable = getContentResolver().query(VideoGamesEntry.CONTENT_URI, projection, null, null, null, null);
 
-        /*Sets up ListView and attaches Cursor Adaptor to it, and tells Adator which Cursor it'll interpret*/
-        ListView listView = (ListView) findViewById(R.id.add_collectable_search_list_view);
-        videoGameCursorAdaptor = new VideoGameCursorAdaptor(this, queryForWholeTable);
-        listView.setAdapter(videoGameCursorAdaptor);
+        /*Sets up ListView and attaches Cursor Adaptor to it, and tells Adaptor which Cursor it'll interpret*/
+        ListView listView = (ListView) findViewById(R.id.collectables_list_view);
+        collectablesCursorAdaptor = new CollectablesCursorAdaptor(this, getTable);
+        listView.setAdapter(collectablesCursorAdaptor);
+
+        getSupportLoaderManager().initLoader(0, null, this);
     }
 
     /*Sets up Options Menu and extracts query from search widget*/
@@ -60,7 +66,7 @@ public class AddCollectableSearchResultsActivity extends AppCompatActivity {
         SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                Intent intent = new Intent(getApplicationContext(), AddCollectableSearchResultsActivity.class);
+                Intent intent = new Intent(getApplicationContext(), AddCollectableActivity.class);
                 intent.putExtra(SEARCH_QUERY, query);
                 intent.setAction(Intent.ACTION_SEARCH);
                 startActivity(intent);
@@ -83,7 +89,7 @@ public class AddCollectableSearchResultsActivity extends AppCompatActivity {
         setIntent(intent);
         /*Should update listView instantiated in onCreate()*/
         Cursor queryResults = handleIntent(intent);
-        videoGameCursorAdaptor.changeCursor(queryResults);
+        collectablesCursorAdaptor.changeCursor(queryResults);
     }
 
     /*Handles query*/
@@ -99,5 +105,21 @@ public class AddCollectableSearchResultsActivity extends AppCompatActivity {
         } else {
             return null;
         }
+    }
+
+    /*Unimplemented loader methods*/
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, VideoGamesEntry.CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        collectablesCursorAdaptor.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        collectablesCursorAdaptor.swapCursor(null);
     }
 }
