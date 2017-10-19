fe11b66f30c9c7f2e2af3225317dd4246fe3b645
Shalom Halbert <shalomhalbert@gmail.com>
2017-10-19 14:07:22 -0400

    Added Listeners for handling Firebase database
    Implemented ChildEventListener and ValueEventListener

--- app/src/main/java/com/example/android/gamecollector/MainActivity.java
+++ app/src/main/java/com/example/android/gamecollector/MainActivity.java
@@ -2,46 +2,123 @@ package com.example.android.gamecollector;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ListView;
+
+import com.example.android.gamecollector.data.firebase.CollectedArrayAdapter;
+import com.example.android.gamecollector.data.firebase.CollectedVideoGame;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * Created by shalom on 2017-10-05.
+ * MainActivity which displays user's personal collection
  */
 
 public class MainActivity extends AppCompatActivity{
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
-
-    // Write a message to the Realtime Database
-//    FirebaseDatabase database;
-//    DatabaseReference personalCollection;
+    /*Contains a list of objects with video game data that will be adapted to a ListView*/
+    private ArrayList<CollectedVideoGame> videoGames = new ArrayList<>();
+    /*Handles videoGames list*/
+    private CollectedArrayAdapter adapter;
+    /*Works with adapter to display data from videoGames*/
+    private ListView listView;
+    /*Instantiation of Realtime Database objects*/
+    private FirebaseDatabase database;
+    private DatabaseReference collectionRef;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createDatabaseListeners();
+
         floatingActionButtonListner();
-
-        //Use for writing to the database
-//        database = FirebaseDatabase.getInstance();
-//        personalCollection = database.getReference().child("personal_collection");
-
     }
 
-    /*Handles tapping the FloatingActionButton by starting AddCollectableSearchResultsActivity*/
+    /*Handles tapping the FloatingActionButton by starting AddCollectableActivity*/
     private void floatingActionButtonListner() {
         FloatingActionButton floatingActionButton = (FloatingActionButton)
-                findViewById(R.id.add_collectable_floating_action_button);
+                findViewById(R.id.add_floating_action_button);
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startAddCollectableSearchResultsActivity = new Intent(getApplicationContext(),
-                        AddCollectableSearchResultsActivity.class);
-                startActivity(startAddCollectableSearchResultsActivity);
+                Intent changeActivity = new Intent(getApplicationContext(),
+                        AddCollectableActivity.class);
+                startActivity(changeActivity);
+            }
+        });
+    }
+
+    /*Initializes a ChildEventListener and ValueEventListener, and adapts videoGames to adapter,
+    * and sets adapter as listView's adapter*/
+    private void createDatabaseListeners() {
+        database = FirebaseDatabase.getInstance();
+        collectionRef = database.getReference().child("collectables_owned").child("video_games");
+
+        /*Used to receive events about changes in the child locations of a given DatabaseReference ref.
+        Attach the listener to a location using addChildEventListener(ChildEventListener) and the
+        appropriate method will be triggered when changes occur.*/
+        collectionRef.addChildEventListener(new ChildEventListener() {
+            /*Creates CollectedVideoGame objects and adds them to the videoGames ArrayList.
+            * Runs after onCreate finishes.*/
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                /*Data points from each video_games node*/
+                int id = dataSnapshot.child("_id").getValue(Integer.class);
+                String console = dataSnapshot.child("console").getValue(String.class);
+                String title = dataSnapshot.child("title").getValue(String.class);
+                String licensee = dataSnapshot.child("licensee").getValue(String.class);
+                String released = dataSnapshot.child("released").getValue(String.class);
+
+                videoGames.add(new CollectedVideoGame(id, console, title, licensee, released));
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        /*Used to receive events about data changes at a location given a DatabaseReference.*/
+        collectionRef.addValueEventListener(new ValueEventListener() {
+            /*Gives videoGames as argument to adapter, and sets adapter as listView's adapter.
+            * Runs after ChildEventListener finishes.*/
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listView = (ListView) findViewById(R.id.list_view_activity_main);
+                adapter = new CollectedArrayAdapter(getApplicationContext(), videoGames);
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
