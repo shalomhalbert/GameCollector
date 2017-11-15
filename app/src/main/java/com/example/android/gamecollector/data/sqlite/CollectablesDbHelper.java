648606fd169ba630081c95b0801d8514a0d47b53
Shalom Halbert <shalomhalbert@gmail.com>
2017-11-15 15:14:48 -0500

    Updated variables and comments, added getUniqueID
    - Due to changes to Contract class's row ID variable name, updates were
    necessary.
    - Comments were reworded for greater clarity
    - Insert SQL statement expanded to add new rows
    - getUniqueID() was created to create the unique ID by concatenating
    a game's console name and title
    - Concatenating required removing characters which are not numbers or
    letters

--- app/src/main/java/com/example/android/gamecollector/data/sqlite/CollectablesDbHelper.java
+++ app/src/main/java/com/example/android/gamecollector/data/sqlite/CollectablesDbHelper.java
@@ -21,17 +21,19 @@ public class CollectablesDbHelper extends SQLiteOpenHelper {
     /*Database version as of 2017-10-07*/
     public static final int DATABASE_VERION = 1;
 
+
     /*Used for creating table with all data*/
     private static final String SQL_CREATE_VIDEO_GAMES_TABLE = "CREATE TABLE "
             + CollectablesSQLContract.VideoGamesEntry.TABLE_NAME + " ("
-            + CollectablesSQLContract.VideoGamesEntry.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + CollectablesSQLContract.VideoGamesEntry.COLUMN_ROW_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + CollectablesSQLContract.VideoGamesEntry.COLUMN_CONSOLE + " TEXT NOT NULL, "
             + CollectablesSQLContract.VideoGamesEntry.COLUMN_TITLE + " TEXT NOT NULL, "
             + CollectablesSQLContract.VideoGamesEntry.COLUMN_LICENSEE + " TEXT DEFAULT unknown, "
-            + CollectablesSQLContract.VideoGamesEntry.COLUMN_RELEASED + " TEXT DEFAULT unknown);";
+            + CollectablesSQLContract.VideoGamesEntry.COLUMN_RELEASED + " TEXT DEFAULT unknown, "
+            + CollectablesSQLContract.VideoGamesEntry.COLUMN_COPIES_OWNED + " , INTEGER DEFAULT 0"
+            + CollectablesSQLContract.VideoGamesEntry.COLUMN_UNIQUE_ID + " TEXT NOT NULL);";
     /*Deletes video_games table*/
     private static final String SQL_DELETE_VIDEO_GAMES_TABLE = "DROP TABLE IF EXISTS " + CollectablesSQLContract.VideoGamesEntry.TABLE_NAME;
-
     /*Used for creating virtual table that uses fts4*/
     private static final String SQL_CREATE_FTS_VIDEO_GAMES_TABLE = "CREATE VIRTUAL TABLE "
             + CollectablesSQLContract.FtsVideoGamesEntry.TABLE_NAME + " USING " + CollectablesSQLContract.FtsVideoGamesEntry.FTS_VERSION
@@ -40,12 +42,13 @@ public class CollectablesDbHelper extends SQLiteOpenHelper {
     /*Used for populating fts_video_games table with data from video_games table*/
     private static final String SQL_POPULATE_FTS_VIDEO_GAMES_TABLE = "INSERT INTO "
             + CollectablesSQLContract.FtsVideoGamesEntry.TABLE_NAME + " (" + CollectablesSQLContract.FtsVideoGamesEntry.COLUMN_DOC_ID + ", "
-            + CollectablesSQLContract.FtsVideoGamesEntry.COLUMN_TITLE + ") SELECT " + CollectablesSQLContract.VideoGamesEntry.COLUMN_ID + ", "
+            + CollectablesSQLContract.FtsVideoGamesEntry.COLUMN_TITLE + ") SELECT " + CollectablesSQLContract.VideoGamesEntry.COLUMN_ROW_ID + ", "
             + CollectablesSQLContract.VideoGamesEntry.COLUMN_TITLE + " FROM " + CollectablesSQLContract.VideoGamesEntry.TABLE_NAME + ";";
     /*Rebuilds fts_video_games table everytime the video_games table is updated*/
     private static final String SQL_REBUILD_FTS_VIDEO_GAMES_TABLE = "INSERT INTO "
             + CollectablesSQLContract.FtsVideoGamesEntry.TABLE_NAME + "(" + CollectablesSQLContract.FtsVideoGamesEntry.TABLE_NAME
             + ") VALUES ('rebuild')";
+
 
     private ParseCSV parseCSV;
     private Context context;
@@ -74,29 +77,122 @@ public class CollectablesDbHelper extends SQLiteOpenHelper {
         db.execSQL(SQL_REBUILD_FTS_VIDEO_GAMES_TABLE);
     }
 
-    /*Parse .csv files and add each line to the database*/
+    /*Parse .csv files and populate database*/
     private void populateVideoGames(SQLiteDatabase db) {
         parseCSV = new ParseCSV(context.getApplicationContext());
 
-        for(List<String> game : parseCSV.parseFiles()) {
+        for (List<String> game : parseCSV.parseFiles()) {
             //Extract data for each game as a String
             String console = game.get(0);
             String title = game.get(1);
             String licensee = game.get(2);
             String released = game.get(3);
+            String uniqueID = getUniqueID(console, title);
 
             /*Handles titles that contain an apostrophe, which must be escaped before use in a SQL statement*/
-            if(title.contains("'")) {
+            if (title.contains("'")) {
                 title = title.replace("'", "''");
             }
 
             /*SQL script used for inserting each video game into the VideoGames table*/
-            String SQL_INSERT_VIDEO_GAMES = "INSERT INTO " + CollectablesSQLContract.VideoGamesEntry.TABLE_NAME
-                    + " (" + CollectablesSQLContract.VideoGamesEntry.COLUMN_CONSOLE + ", " + CollectablesSQLContract.VideoGamesEntry.COLUMN_TITLE
-                    + ", " + CollectablesSQLContract.VideoGamesEntry.COLUMN_LICENSEE + ", " + CollectablesSQLContract.VideoGamesEntry.COLUMN_RELEASED
-                    + ") VALUES ('" + console + "', '" + title + "', '" + licensee + "', '" + released + "');";
+            String SQL_INSERT_VIDEO_GAMES = "INSERT INTO " + CollectablesSQLContract.VideoGamesEntry.TABLE_NAME + " ("
+                    + CollectablesSQLContract.VideoGamesEntry.COLUMN_CONSOLE + ", "
+                    + CollectablesSQLContract.VideoGamesEntry.COLUMN_TITLE + ", "
+                    + CollectablesSQLContract.VideoGamesEntry.COLUMN_LICENSEE + ", "
+                    + CollectablesSQLContract.VideoGamesEntry.COLUMN_RELEASED + ", "
+                    + CollectablesSQLContract.VideoGamesEntry.COLUMN_UNIQUE_ID
+                    + ") VALUES ('" + console + "', '" + title + "', '" + licensee + "', '" + released + "', '" + uniqueID + "');";
 
             db.execSQL(SQL_INSERT_VIDEO_GAMES);
+        }
+    }
+
+    /*Concatenates console and title into one String, and removes all characters which are not a letter or number*/
+    private String getUniqueID(String console, String title) {
+        /*Unconcatenated version of uniqueID*/
+        String longUniqueID = console + title;
+        StringBuilder uniqueID = new StringBuilder();
+
+        /*If the char is a number or letter, append it to the string*/
+        for (int i=0; i < longUniqueID.length(); i++) {
+            if (handleChars(longUniqueID.charAt(i))) {
+               uniqueID.append(longUniqueID.charAt(i));
+            }
+        }
+        return uniqueID.toString().toLowerCase();
+    }
+
+    /*'true' means the char is a number or letter*/
+    private boolean handleChars(char x) {
+        switch (x) {
+            case '`':
+                return false;
+            case '~':
+                return false;
+            case '!':
+                return false;
+            case '@':
+                return false;
+            case '#':
+                return false;
+            case '$':
+                return false;
+            case '%':
+                return false;
+            case '^':
+                return false;
+            case '&':
+                return false;
+            case '*':
+                return false;
+            case '(':
+                return false;
+            case ')':
+                return false;
+            case '-':
+                return false;
+            case '_':
+                return false;
+            case '+':
+                return false;
+            case '=':
+                return false;
+            case '[':
+                return false;
+            case ']':
+                return false;
+            case '{':
+                return false;
+            case '}':
+                return false;
+            case '\\':
+                return false;
+            case '|':
+                return false;
+            case ';':
+                return false;
+            case ':':
+                return false;
+            case '\'':
+                return false;
+            case '"':
+                return false;
+            case ',':
+                return false;
+            case '.':
+                return false;
+            case '<':
+                return false;
+            case '>':
+                return false;
+            case '/':
+                return false;
+            case '?':
+                return false;
+            case ' ':
+                return false;
+            default:
+                return true;
         }
     }
 }
