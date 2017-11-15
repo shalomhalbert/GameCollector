e80e4b22e0341d7b6286ce4aae905b5b2e18f9bd
Shalom Halbert <shalomhalbert@gmail.com>
2017-11-15 15:20:30 -0500

    Update query String, format code, draft update()
    Formatting helps with reading code.
    Drafting update() is for applying increases to CopiesOwned column

--- app/src/main/java/com/example/android/gamecollector/data/sqlite/CollectablesProvider.java
+++ app/src/main/java/com/example/android/gamecollector/data/sqlite/CollectablesProvider.java
@@ -33,14 +33,20 @@ public class CollectablesProvider extends ContentProvider {
      * the resulting row's 'docid' that is in the indexed fts_video_games table*/
     private static final String SQL_QUERY_ADD_COLLECTABLE_SEARCH_RESULTS_ACTIVITY = "SELECT * FROM "
             + CollectablesSQLContract.VideoGamesEntry.TABLE_NAME + " WHERE "
-            + CollectablesSQLContract.VideoGamesEntry.COLUMN_ID + " IN (SELECT docid FROM " + CollectablesSQLContract.FtsVideoGamesEntry.TABLE_NAME + " WHERE " + CollectablesSQLContract.FtsVideoGamesEntry.TABLE_NAME
+            + CollectablesSQLContract.VideoGamesEntry.COLUMN_ROW_ID
+            + " IN (SELECT docid FROM " + CollectablesSQLContract.FtsVideoGamesEntry.TABLE_NAME
+            + " WHERE " + CollectablesSQLContract.FtsVideoGamesEntry.TABLE_NAME
             + " MATCH ?)";
 
     static {
-        URI_MATCHER.addURI(CollectablesSQLContract.CONTENT_AUTHORITY, CollectablesSQLContract.PATH_VIDEO_GAMES, VIDEO_GAMES);
-        URI_MATCHER.addURI(CollectablesSQLContract.CONTENT_AUTHORITY, CollectablesSQLContract.PATH_VIDEO_GAMES + "/#", VIDEO_GAME_ID);
-        URI_MATCHER.addURI(CollectablesSQLContract.CONTENT_AUTHORITY, CollectablesSQLContract.PATH_FTS_VIDEO_GAMES, FTS_VIDEO_GAMES);
-        URI_MATCHER.addURI(CollectablesSQLContract.CONTENT_AUTHORITY, CollectablesSQLContract.PATH_FTS_VIDEO_GAMES + "/#", FTS_VIDEO_GAMES_ID);
+        URI_MATCHER.addURI(CollectablesSQLContract.CONTENT_AUTHORITY,
+                CollectablesSQLContract.PATH_VIDEO_GAMES, VIDEO_GAMES);
+        URI_MATCHER.addURI(CollectablesSQLContract.CONTENT_AUTHORITY,
+                CollectablesSQLContract.PATH_VIDEO_GAMES + "/#", VIDEO_GAME_ID);
+        URI_MATCHER.addURI(CollectablesSQLContract.CONTENT_AUTHORITY,
+                CollectablesSQLContract.PATH_FTS_VIDEO_GAMES, FTS_VIDEO_GAMES);
+        URI_MATCHER.addURI(CollectablesSQLContract.CONTENT_AUTHORITY,
+                CollectablesSQLContract.PATH_FTS_VIDEO_GAMES + "/#", FTS_VIDEO_GAMES_ID);
     }
 
     @Override
@@ -50,23 +56,28 @@ public class CollectablesProvider extends ContentProvider {
     }
 
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
         sqLiteDatabase = collectablesDbHelper.getReadableDatabase();
         Cursor cursor;
 
         switch (URI_MATCHER.match(uri)) {
             case VIDEO_GAMES:
-                cursor = sqLiteDatabase.query(CollectablesSQLContract.VideoGamesEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                cursor = sqLiteDatabase.query(CollectablesSQLContract.VideoGamesEntry.TABLE_NAME,
+                        projection, selection, selectionArgs, null, null, sortOrder);
                 break;
             case VIDEO_GAME_ID:
                 /*SQL statement for extracting a single row from the video_games table using row ID*/
-                String sqlString = "SELECT * FROM " + CollectablesSQLContract.VideoGamesEntry.TABLE_NAME + " WHERE " + CollectablesSQLContract.VideoGamesEntry.COLUMN_ID + "=?";
+                String sqlString = "SELECT * FROM "
+                        + CollectablesSQLContract.VideoGamesEntry.TABLE_NAME
+                        + " WHERE " + CollectablesSQLContract.VideoGamesEntry.COLUMN_ROW_ID + "=?";
                 /*Cursor containing all data for the selected row*/
                 cursor = sqLiteDatabase.rawQuery(sqlString, selectionArgs);
                 break;
             case FTS_VIDEO_GAMES:
                 /*Enables selectionArgs to filter row selection*/
-                cursor = sqLiteDatabase.rawQuery(SQL_QUERY_ADD_COLLECTABLE_SEARCH_RESULTS_ACTIVITY, selectionArgs);
+                cursor = sqLiteDatabase.rawQuery(SQL_QUERY_ADD_COLLECTABLE_SEARCH_RESULTS_ACTIVITY,
+                        selectionArgs);
                 break;
             case FTS_VIDEO_GAMES_ID:
                 /*Add actions once this case is a possiblity*/
@@ -76,7 +87,7 @@ public class CollectablesProvider extends ContentProvider {
                 throw new IllegalStateException("Cannot query unkown URI " + uri);
         }
 
-        cursor.setNotificationUri(getContext().getContentResolver(),uri);
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
 
         return cursor;
     }
@@ -84,7 +95,7 @@ public class CollectablesProvider extends ContentProvider {
     /*Returns MIME type of the given Uri argument*/
     @Override
     public String getType(Uri uri) {
-        switch(URI_MATCHER.match(uri)) {
+        switch (URI_MATCHER.match(uri)) {
             case VIDEO_GAMES:
                 return CollectablesSQLContract.VideoGamesEntry.CONTENT_TYPE;
             case VIDEO_GAME_ID:
@@ -100,15 +111,31 @@ public class CollectablesProvider extends ContentProvider {
         return null;
     }
 
-    /*Users will not delete data into this database*/
+    /*Runs update and returns the number of rows affected*/
     @Override
-    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        int rowsUpdated = 0;
+
+        switch (URI_MATCHER.match(uri)) {
+            case VIDEO_GAMES:
+                break;
+            case VIDEO_GAME_ID:
+                /*Updates a row based on a given Unique_ID*/
+                rowsUpdated = sqLiteDatabase.update(CollectablesSQLContract.VideoGamesEntry.TABLE_NAME,
+                        values,
+                        CollectablesSQLContract.VideoGamesEntry.COLUMN_UNIQUE_ID + "=?",
+                        selectionArgs);
+                break;
+            default:
+                throw new IllegalStateException("Cannot update unkown URI " + uri);
+        }
+
+        return rowsUpdated;
     }
 
-    /*Users will not update data into this database*/
+    /*Users will not delete data into this database*/
     @Override
-    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
         return 0;
     }
 }
