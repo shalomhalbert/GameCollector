af025f1671d1fcf937af62ee201f76df76f033c8
Shalom Halbert <shalomhalbert@gmail.com>
2017-11-20 17:28:47 -0500

    Created class for storing a video game's data

---
+++ app/src/main/java/com/example/android/gamecollector/collectable/videoGames/VideoGame.java
@@ -0,0 +1,156 @@
+package com.example.android.gamecollector.collectable.videoGames;
+
+import com.example.android.gamecollector.data.sqlite.CollectableContract;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+/**
+ * Created by shalom on 2017-11-17.
+ * An object whose sole purpose is temproary storage of a single video game's data
+ */
+
+public class VideoGame {
+    /*Data that will be inputted into Firebase Realtime Database*/
+    private String uniqueID;
+    private String console;
+    private String title;
+    private String licensee;
+    private String released;
+    private String dateAdded;
+    private String regionLock;
+    private HashMap<String,Boolean> componentsOwned = new HashMap<>();
+    private String note;
+
+    /*Constants which can be regionLock's value*/
+    public static final String USA = "USA";
+    public static final String JAPAN = "Japan";
+    public static final String EUROPEAN_UNION = "EU";
+
+    /*Constants for componentsOwned's keys*/
+    public static final String GAME = "Game";
+    public static final String MANUAL = "Manual";
+    public static final String BOX = "Box";
+
+    /*Constants for other Firebase's key values that are not one of table VideoGames's column names*/
+    public static final String DATE_ADDED = "Date_Added";
+    public static final String REGION_LOCK = "Region_Lock";
+    public static final String COMPONENTS_OWNED = "Components_Owned";
+    public static final String NOTE = "Note";
+
+    public static final String UNDEFINED_TRAIT = "undefined";
+
+    /*Firbase Realtime Database initializations*/
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
+
+    public VideoGame(String uniqueID, String console, String title, String licensee, String released) {
+        this.uniqueID = uniqueID;
+        this.console = console;
+        this.title = title;
+        this.licensee = licensee;
+        this.released = released;
+    }
+
+    public void saveToFirebase() {
+        /*Create a unique ID that names a node for an individual video game when it's added*/
+        String uniqueNodeId = UUID.randomUUID().toString();
+
+        if (regionLock == null) {
+            regionLock = UNDEFINED_TRAIT;
+        }
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference()
+                .child("collectables_owned")
+                .child("video_games")
+                .child(uniqueNodeId);
+
+        databaseReference.child(CollectableContract.VideoGamesEntry.COLUMN_UNIQUE_ID).setValue(uniqueID);
+        databaseReference.child(CollectableContract.VideoGamesEntry.COLUMN_CONSOLE).setValue(console);
+        databaseReference.child(CollectableContract.VideoGamesEntry.COLUMN_TITLE).setValue(title);
+        databaseReference.child(CollectableContract.VideoGamesEntry.COLUMN_LICENSEE).setValue(licensee);
+        databaseReference.child(CollectableContract.VideoGamesEntry.COLUMN_RELEASED).setValue(released);
+        databaseReference.child(DATE_ADDED).setValue(dateAdded);
+        databaseReference.child(REGION_LOCK).setValue(regionLock);
+        databaseReference.child(COMPONENTS_OWNED).child(GAME).setValue(componentsOwned.get(GAME).toString());
+        databaseReference.child(COMPONENTS_OWNED).child(MANUAL).setValue(componentsOwned.get(MANUAL).toString());
+        databaseReference.child(COMPONENTS_OWNED).child(BOX).setValue(componentsOwned.get(BOX).toString());
+        databaseReference.child(NOTE).setValue(note);
+    }
+
+    public void setUniqueID(String uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+
+    public void setConsole(String console) {
+        this.console = console;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setLicensee(String licensee) {
+        this.licensee = licensee;
+    }
+
+    public void setReleased(String released) {
+        this.released = released;
+    }
+
+    public void setDateAdded(String dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public void setRegionLock(String regionLock) {
+        this.regionLock = regionLock;
+    }
+
+    public void setComponentsOwned(HashMap<String,Boolean> componentsOwned) {
+        this.componentsOwned = componentsOwned;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getUniqueID() {
+        return uniqueID;
+    }
+
+    public String getConsole() {
+        return console;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLicensee() {
+        return licensee;
+    }
+
+    public String getReleased() {
+        return released;
+    }
+
+    public String getDateAdded() {
+        return dateAdded;
+    }
+
+    public String getRegionLock() {
+        return regionLock;
+    }
+
+    public HashMap<String, Boolean> getComponentsOwned() {
+        return componentsOwned;
+    }
+
+    public String getNote() {
+        return note;
+    }
+}
