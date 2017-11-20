fbd7fe6a5554ef3f1f31be544bc21aedd049aa22
Shalom Halbert <shalomhalbert@gmail.com>
2017-11-20 17:44:15 -0500

    Setup Toolbar; Gave save action button its functionality
    - Added pre-onCreate() instantiations
    - Overrode onCreate() which extracts data from a Bundle
    - Initialized and setup Toolbar as ActionBar, and setup options menu
    - Gave functionality to save action button from ActionBar
    - Modified onClickListeners for passing data to VideoGame object
    - Created populateVideoGame()
    - Deleted returnToMainActivity() and handleButtons()

--- app/src/main/java/com/example/android/gamecollector/collectable/videoGames/CollectableDialogFragment.java
+++ app/src/main/java/com/example/android/gamecollector/collectable/videoGames/CollectableDialogFragment.java
@@ -1,15 +1,17 @@
 package com.example.android.gamecollector.collectable.videoGames;
 
 import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.PorterDuff;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,16 +19,23 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 
 import com.example.android.gamecollector.R;
-import com.example.android.gamecollector.data.sqlite.CollectableSQLContract;
 import com.example.android.gamecollector.collected.videoGames.PersonalCollectionActivity;
+import com.example.android.gamecollector.data.sqlite.CollectableContract;
+import com.example.android.gamecollector.data.sqlite.CollectableContract.VideoGamesEntry;
+import com.example.android.gamecollector.data.sqlite.CollectableCursorAdaptor;
+import com.example.android.gamecollector.data.sqlite.CollectableProvider;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -38,21 +47,68 @@ import java.util.UUID;
  */
 
 public class CollectableDialogFragment extends DialogFragment {
-    Context context;
+    public static final String LOG_TAG = CollectableDialogFragment.class.getSimpleName();
+    private Context context;
+    /*Map containing video game data of the clicked list item*/
+    private HashMap<String, String> videoGameData;
+    /*Object containing video game's data*/
+    private VideoGame videoGame;
+    /*String naming which region video game is locked to*/
+    private String regionLock = null;
+    /*Map of boolean values with keys for the possible components and values that realy whether the user has the component
+     *True means the component is owned*/
+    private HashMap<String, Boolean> componentsOwned = new HashMap<>();
+    /**/
+    private EditText note;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            /*Extract Hashmap containing video game's data*/
+            videoGameData = (HashMap<String, String>) getArguments().getSerializable(CollectableProvider.VIDEO_GAME_DATA);
+        }
+
+        componentsOwned.put(VideoGame.GAME, false);
+        componentsOwned.put(VideoGame.MANUAL, false);
+        componentsOwned.put(VideoGame.BOX, false);
+    }
 
     /*Inflate the layout to use as dialog or embedded fragment*/
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_collectable_dialog, container, false);
 
+//        String toolbarTitle = R.string.fragment_collectable_title + "[add collectable's title]";
 
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.activity_collectable_dialog_toolbar);
+//        toolbar.setTitle(toolbarTitle);
+        /*Sets toolbar as ActionBar*/
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        /*Enable home button and supply a custom icon*/
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_clear_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        note = (EditText) view.findViewById(R.id.activity_collectable_edittext_notes);
+
+        /*Report that this fragment would like to participate in populating the options menu by
+        receiving a call to onCreateOptionsMenu(Menu, MenuInflater) and related methods.*/
+        setHasOptionsMenu(true);
 
         return view;
     }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return super.onCreateDialog(savedInstanceState);
+        /*Causes dialog to cover whole screen*/
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
     }
 
     /*Inflates activity_collectable_dialog_menu as the actionbar*/
@@ -64,6 +120,20 @@ public class CollectableDialogFragment extends DialogFragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 //        setup switch-case for each action
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.activity_collectable_dialog_action_save:
+//                Update SQLite database Copies_Owned
+                videoGame = populateVideoGame();
+
+
+                break;
+            case android.R.id.home ;
+//                Make sure to confirm discard of data if data was input
+                dismiss();
+                break;
+        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -104,46 +174,40 @@ public class CollectableDialogFragment extends DialogFragment {
     /*Initializes and handles onClickListeners for responding to when flags are tapped*/
     private void setRegionLock(final ImageView usaFlag, final ImageView japanFlag, final ImageView euFlag) {
         usaFlag.setOnClickListener(new View.OnClickListener() {
-            boolean activated = false;
-
             @Override
             public void onClick(View v) {
-                if (activated == false) {
+                if (regionLock != VideoGame.USA) {
                     setSingleIconAsActive(usaFlag, japanFlag, euFlag);
-                    activated = true;
-                } else if (activated == true) {
+                    regionLock = VideoGame.USA;
+                } else if (regionLock == VideoGame.USA) {
                     setIconAsInactive(usaFlag);
-                    activated = false;
+                    regionLock = null;
                 }
             }
         });
 
         japanFlag.setOnClickListener(new View.OnClickListener() {
-            boolean activated = false;
-
             @Override
             public void onClick(View v) {
-                if (activated == false) {
+                if (regionLock != VideoGame.JAPAN) {
                     setSingleIconAsActive(japanFlag, usaFlag, euFlag);
-                    activated = true;
-                } else if (activated == true) {
+                    regionLock = VideoGame.JAPAN;
+                } else if (regionLock == VideoGame.JAPAN) {
                     setIconAsInactive(japanFlag);
-                    activated = false;
+                    regionLock = null;
                 }
             }
         });
 
         euFlag.setOnClickListener(new View.OnClickListener() {
-            boolean activated = false;
-
             @Override
             public void onClick(View v) {
-                if (activated == false) {
+                if (regionLock != VideoGame.EUROPEAN_UNION) {
                     setSingleIconAsActive(euFlag, usaFlag, japanFlag);
-                    activated = true;
-                } else if (activated == true) {
+                    regionLock = VideoGame.EUROPEAN_UNION;
+                } else if (regionLock == VideoGame.EUROPEAN_UNION) {
                     setIconAsInactive(euFlag);
-                    activated = false;
+                    regionLock = null;
                 }
             }
         });
@@ -152,46 +216,40 @@ public class CollectableDialogFragment extends DialogFragment {
     /*Initializes and handles onClickListeners for responding to when components are tapped*/
     private void setComponentsOwned(final ImageView game, final ImageView manual, final ImageView box) {
         game.setOnClickListener(new View.OnClickListener() {
-            boolean activated = false;
-
             @Override
             public void onClick(View v) {
-                if (activated == false) {
+                if (componentsOwned.get(VideoGame.GAME) == false) {
                     setIconAsActive(game);
-                    activated = true;
-                } else if (activated == true) {
+                    componentsOwned.put(VideoGame.GAME, true);
+                } else if (componentsOwned.get(VideoGame.GAME) == true) {
                     setIconAsInactive(game);
-                    activated = false;
+                    componentsOwned.put(VideoGame.GAME, false);
                 }
             }
         });
 
         manual.setOnClickListener(new View.OnClickListener() {
-            boolean activated = false;
-
             @Override
             public void onClick(View v) {
-                if (activated == false) {
+                if (componentsOwned.get(VideoGame.MANUAL) == false) {
                     setIconAsActive(manual);
-                    activated = true;
-                } else if (activated == true) {
+                    componentsOwned.put(VideoGame.GAME, true);
+                } else if (componentsOwned.get(VideoGame.MANUAL) == true) {
                     setIconAsInactive(manual);
-                    activated = false;
+                    componentsOwned.put(VideoGame.GAME, false);
                 }
             }
         });
 
         box.setOnClickListener(new View.OnClickListener() {
-            boolean activated = false;
-
             @Override
             public void onClick(View v) {
-                if (activated == false) {
+                if (componentsOwned.get(VideoGame.BOX) == false) {
                     setIconAsActive(box);
-                    activated = true;
-                } else if (activated == true) {
+                    componentsOwned.put(VideoGame.GAME, true);
+                } else if (componentsOwned.get(VideoGame.MANUAL) == true) {
                     setIconAsInactive(box);
-                    activated = false;
+                    componentsOwned.put(VideoGame.GAME, false);
                 }
             }
         });
@@ -216,101 +274,25 @@ public class CollectableDialogFragment extends DialogFragment {
         imageView.setColorFilter(ContextCompat.getColor(getView().getContext(), R.color.colorInactiveIcon), PorterDuff.Mode.SRC_IN);
     }
 
+    /*Populates VideoGame object*/
+    private VideoGame populateVideoGame() {
+        /*Get values from videoGameData*/
+        String uniqueId = videoGameData.get(VideoGamesEntry.COLUMN_UNIQUE_ID);
+        String console = videoGameData.get(VideoGamesEntry.COLUMN_CONSOLE);
+        String title = videoGameData.get(VideoGamesEntry.COLUMN_TITLE);
+        String licensee = videoGameData.get(VideoGamesEntry.COLUMN_LICENSEE);
+        String released = videoGameData.get(VideoGamesEntry.COLUMN_RELEASED);
 
-    /*Runs an explicit intent that opens PersonalCollectionActivity*/
-    private void returnToMainActivity(Context context) {
-        Intent intent = new Intent(context.getApplicationContext(), PersonalCollectionActivity.class);
-        context.startActivity(intent);
-    }
+        VideoGame videoGame1 = new VideoGame(uniqueId, console, title, licensee, released);
 
-    /*Handles onClickListeners for buttons in the popup window*/
-    private void handleButtons(final Context context, final Cursor cursor, View popupView, final PopupWindow popupWindow) {
-         /*Initialize response buttons*/
-        Button confirmButton = (Button) popupView.findViewById(R.id.confirm_addition);
-        Button declineButton = (Button) popupView.findViewById(R.id.decline_addition);
+                /*Get current date and time*/
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
 
-        /*If user confirms addition of a collectable item to their collection, it's data will be
-        * add to the Firebase Realtime Database*/
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /*Initialize a Map containing all the values of the cursor*/
-                Map<String, String> cursorData = getItemData(context, cursor);
+        videoGame1.setDateAdded(currentDateTimeString);
+        videoGame1.setRegionLock(regionLock);
+        videoGame1.setComponentsOwned(componentsOwned);
+        videoGame1.setNote(note.getText().toString());
 
-                /*Initialize individual variables for each datapoint in cursor*/
-                String collectableUniqueId = cursorData.get(CollectableSQLContract.VideoGamesEntry.COLUMN_UNIQUE_ID);
-                String collectableConsole = cursorData.get(CollectableSQLContract.VideoGamesEntry.COLUMN_CONSOLE);
-                String collectableTitle = cursorData.get(CollectableSQLContract.VideoGamesEntry.COLUMN_TITLE);
-                String collectableLicensee = cursorData.get(CollectableSQLContract.VideoGamesEntry.COLUMN_LICENSEE);
-                String collectableReleased = cursorData.get(CollectableSQLContract.VideoGamesEntry.COLUMN_RELEASED);
-
-                /*Create a unique ID that names a node for an individual video game when it's added*/
-                String uniqueNodeId = UUID.randomUUID().toString();
-
-                String logInfo = "uniqueNodeId: " + collectableUniqueId
-                        + ", Console: " + collectableConsole
-                        + ", Title: " + collectableTitle;
-                Log.i(LOG_TAG, logInfo);
-
-                firebaseDatabase = FirebaseDatabase.getInstance();
-                databaseReference = firebaseDatabase.getReference()
-                        .child("collectables_owned")
-                        .child("video_games")
-                        .child(uniqueNodeId);
-
-                databaseReference.child(CollectableSQLContract.VideoGamesEntry.COLUMN_UNIQUE_ID).setValue(collectableUniqueId);
-                databaseReference.child(CollectableSQLContract.VideoGamesEntry.COLUMN_CONSOLE).setValue(collectableConsole);
-                databaseReference.child(CollectableSQLContract.VideoGamesEntry.COLUMN_TITLE).setValue(collectableTitle);
-                databaseReference.child(CollectableSQLContract.VideoGamesEntry.COLUMN_LICENSEE).setValue(collectableLicensee);
-                databaseReference.child(CollectableSQLContract.VideoGamesEntry.COLUMN_RELEASED).setValue(collectableReleased);
-
-                returnToMainActivity(context);
-            }
-        });
-
-        /*If user doesn't want the item added to their collection, the popup window disappears*/
-        declineButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                popupWindow.dismiss();
-            }
-        });
-    }
-
-    /*Returns the selected collectable's data as a Map */
-    private Map<String, String> getItemData(final Context context, final Cursor cursor) {
-        /*Map will contain item data*/
-        Map<String, String> map = new HashMap<>();
-
-        /*Get row ID for tapped collectable item*/
-        long rowId = cursor.getInt(cursor.getColumnIndexOrThrow(CollectableSQLContract.VideoGamesEntry.COLUMN_ROW_ID));
-        String[] selectionArgs = {String.valueOf(rowId)};
-        /*Create Uri for the tapped collectable item*/
-        Uri individualItemUri = ContentUris.withAppendedId(CollectableSQLContract.VideoGamesEntry.CONTENT_URI, rowId);
-
-        /*Get cursor with data belonging to the tapped collectable item*/
-        Cursor newCollectable = context.getContentResolver().query(individualItemUri, null, null, selectionArgs, null);
-
-        if (newCollectable != null && newCollectable.moveToFirst()) {
-            /*Get every value from cursor*/
-            //Reformat code to centralize this action
-            String collectableId = newCollectable.getString(newCollectable.getColumnIndexOrThrow(CollectableSQLContract.VideoGamesEntry.COLUMN_ROW_ID));
-            String collectableConsole = newCollectable.getString(newCollectable.getColumnIndexOrThrow(CollectableSQLContract.VideoGamesEntry.COLUMN_CONSOLE));
-            String collectableTitle = newCollectable.getString(newCollectable.getColumnIndexOrThrow(CollectableSQLContract.VideoGamesEntry.COLUMN_TITLE));
-            String collectableLicensee = newCollectable.getString(newCollectable.getColumnIndexOrThrow(CollectableSQLContract.VideoGamesEntry.COLUMN_LICENSEE));
-            String collectableReleased = newCollectable.getString(newCollectable.getColumnIndexOrThrow(CollectableSQLContract.VideoGamesEntry.COLUMN_RELEASED));
-
-            /*Add values to Map*/
-            map.put(_ID, collectableId);
-            map.put(CONSOLE, collectableConsole);
-            map.put(TITLE, collectableTitle);
-            map.put(LICENSEE, collectableLicensee);
-            map.put(RELEASED, collectableReleased);
-        } else {
-            Log.e(LOG_TAG, "Problem getting cursor values");
-            return null;
-        }
-
-        return map;
+        return videoGame1;
     }
 }
