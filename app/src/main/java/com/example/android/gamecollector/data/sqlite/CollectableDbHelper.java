9e36550b81ec9a9c0616e522bdbfa55212a6745b
Shalom Halbert <shalomhalbert@gmail.com>
2017-12-06 15:32:18 -0500

    Effected by moving ParseCSV

--- app/src/main/java/com/example/android/gamecollector/data/sqlite/CollectableDbHelper.java
+++ app/src/main/java/com/example/android/gamecollector/data/sqlite/CollectableDbHelper.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.example.android.gamecollector.ParseCSV;
-
 import java.util.List;
 
 /**
