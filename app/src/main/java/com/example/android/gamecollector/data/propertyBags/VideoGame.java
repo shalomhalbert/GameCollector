7386d79bd9484181c749a5909264350b64210ee1
Shalom Halbert <shalomhalbert@gmail.com>
2018-01-12 11:37:33 -0500

    Reorganized classes into new packages

--- app/src/main/java/com/example/android/gamecollector/data/propertyBags/VideoGame.java
+++ app/src/main/java/com/example/android/gamecollector/data/propertyBags/VideoGame.java
@@ -1,18 +1,12 @@
 package com.example.android.gamecollector;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.net.Uri;
-import android.util.Log;
 
 import com.example.android.gamecollector.data.sqlite.CollectableContract;
-import com.example.android.gamecollector.data.sqlite.CollectableContract.VideoGamesEntry;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
-import java.util.UUID;
 /**
  * Created by shalom on 2017-11-17.
  * An object whose sole purpose is temproary storage of a single video game's data
@@ -64,6 +58,7 @@ public class VideoGame {
     private int valueCopiesOwned = 0;
     /*Constant names of database nodes*/
     public static final String NODE_COLLECTABLES_OWNED = "collectables_owned";
+    public static final String NODE_USERS = "users";
     public static final String NODE_VIDEO_GAMES = "video_games";
     /*Firebase Realtime Database initializations*/
     private FirebaseDatabase firebaseDatabase;
@@ -98,88 +93,6 @@ public class VideoGame {
         this.valueUniqueNodeId = valueUniqueNodeId;
     }
 
-    public void createNode() {
-        /*Create a unique ID that names a node for an individual video game when it's added*/
-        valueUniqueNodeId = UUID.randomUUID().toString();
-
-        if (valueRegionLock == null) {
-            valueRegionLock = UNDEFINED_TRAIT;
-        }
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference()
-                .child(NODE_COLLECTABLES_OWNED)
-                .child(NODE_VIDEO_GAMES)
-                .child(valueUniqueNodeId);
-
-        databaseReference.child(KEY_UNIQUE_ID).setValue(valueUniqueID);
-        databaseReference.child(KEY_CONSOLE).setValue(valueConsole);
-        databaseReference.child(KEY_TITLE).setValue(valueTitle);
-        databaseReference.child(KEY_LICENSEE).setValue(valueLicensee);
-        databaseReference.child(KEY_RELEASED).setValue(valueReleased);
-        databaseReference.child(KEY_DATE_ADDED).setValue(valueDateAdded);
-        databaseReference.child(KEY_REGION_LOCK).setValue(valueRegionLock);
-        databaseReference.child(KEY_COMPONENTS_OWNED).child(GAME).setValue(valuesComponentsOwned.get(GAME));
-        databaseReference.child(KEY_COMPONENTS_OWNED).child(MANUAL).setValue(valuesComponentsOwned.get(MANUAL));
-        databaseReference.child(KEY_COMPONENTS_OWNED).child(BOX).setValue(valuesComponentsOwned.get(BOX));
-        databaseReference.child(KEY_NOTE).setValue(valueNote);
-        databaseReference.child(KEY_UNIQUE_NODE_ID).setValue(valueUniqueNodeId);
-
-        int rowsUpdated = updateCopiesOwned();
-
-        Log.i(LOG_TAG, "Rows updated: " + rowsUpdated);
-    }
-
-    public void updateNode() {
-        Log.i(LOG_TAG, "regionLock: " + getValueRegionLock() + ", game: " + getValueGame() + ", manual: " + getValueManual() + ", box: " + getValueBox());
-        if (valueRegionLock == null) {
-            valueRegionLock = UNDEFINED_TRAIT;
-        }
-
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference()
-                .child(NODE_COLLECTABLES_OWNED)
-                .child(NODE_VIDEO_GAMES)
-                .child(getValueUniqueNodeId());
-        /*Update region lock*/
-        databaseReference.child(KEY_REGION_LOCK).setValue(getValueRegionLock());
-        /*Update components owned*/
-        databaseReference.child(KEY_COMPONENTS_OWNED).child(GAME).setValue(getValueGame());
-        databaseReference.child(KEY_COMPONENTS_OWNED).child(MANUAL).setValue(getValueManual());
-        databaseReference.child(KEY_COMPONENTS_OWNED).child(BOX).setValue(getValueBox());
-        /*Update note*/
-        databaseReference.child(KEY_NOTE).setValue(valueNote);
-
-    }
-
-    private int updateCopiesOwned() {
-        /*Updated number of copes owned*/
-        int updatedCopies = getValueCopiesOwned() + 1;
-        /*Uri with appropriate rowId appended*/
-        Uri uri = Uri.withAppendedPath(VideoGamesEntry.CONTENT_URI, getValueRowID());
-        /*Key value pair used for updating database*/
-        ContentValues sqliteUpdate = new ContentValues();
-        sqliteUpdate.put(VideoGamesEntry.COLUMN_COPIES_OWNED, String.valueOf(updatedCopies));
-
-        String selection = KEY_UNIQUE_ID + "=?";
-        String[] selectionArgs = {getValueUniqueID()};
-
-
-        /*Update SQLite database*/
-       int rowsUpdated = context.getContentResolver().update(uri, sqliteUpdate, selection, selectionArgs);
-
-       return rowsUpdated;
-    }
-
-    /*Removes the video game's node from Firebase*/
-    public void deleteNode() {
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        firebaseDatabase.getReference().child(NODE_COLLECTABLES_OWNED)
-                .child(NODE_VIDEO_GAMES).child(valueUniqueNodeId).removeValue(new DatabaseReference.CompletionListener() {
-                    @Override
-                    public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
-                    }
-                });
-    }
 
     public String getValueRowID() {
         return valueRowID;
